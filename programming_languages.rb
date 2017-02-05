27a6a51194464d7b0987a44ee25f58146ac28165
Rathi Nair-Das <rathind14@gmail.com>
2017-02-05 13:58:12 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = { }
+      languages.each do |key,value|
+        if value.is_a?(Hash)
+          value.each do |n_key,n_value|
+             if n_value.is_a?(Hash)
+               n_value.each do |nn_key,nn_value|
+                 new_hash[n_key] = {nn_key => nn_value}
+               end
+             end
+            if n_key.to_s == "javascript"
+              new_hash[n_key][:style] = [:oo ,key]
+            else
+            new_hash[n_key][:style]  =  [key]
+           end 
+          end
+        end
+      end
+      new_hash
+
 end
